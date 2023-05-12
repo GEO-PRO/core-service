c653c7ed28c37c2cb825221f1da8328a36578da4
huynhthom <thomhuynh20052000lxag@gmail.com>
2023-05-12 14:07:09 +0700

    add upload file

--- src/uploadfile/uploadfile.controller.ts
+++ src/uploadfile/uploadfile.controller.ts
@@ -13,7 +13,7 @@ export class FilesController {
     @Post()
     @UseInterceptors(FileInterceptor('file', {
         storage: diskStorage({
-            destination: '../ui-web/public/images',
+            destination: './images-service/images',
             filename: (req, file, cb) => {
                 const now = moment();
                 const formattedTime = moment(now).format('DD-MM-YYYY');
